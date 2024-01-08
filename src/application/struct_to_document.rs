82c54c00e8a07bbd4994a8bbe94dad0a0595e6bc
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-08 22:04:53 +0100

    UPDATE - struct to document

--- src/application/struct_to_document.rs
+++ src/application/struct_to_document.rs
@@ -1,10 +1,10 @@
-use bson::{ser::Error, to_bson, Bson, Document};
+use bson::{ser::Error as BsonError, to_bson, Bson, Document};
 use serde::{ser::Error as SerError, Serialize};
 
-pub fn struct_to_document<T: Serialize>(data: &T) -> Result<Document, Error> {
+pub fn struct_to_document<T: Serialize>(data: &T) -> Result<Document, BsonError> {
     match to_bson(data) {
         Ok(Bson::Document(document)) => Ok(document),
-        Ok(_) => Err(Error::custom("Expected a BSON document.")),
+        Ok(_) => Err(BsonError::custom("Expected a BSON document.")),
         Err(e) => Err(e),
     }
 }

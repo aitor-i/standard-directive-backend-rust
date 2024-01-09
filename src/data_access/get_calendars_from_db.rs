7a89026ef8c5ccf7d4a6a9d94b2879769181e8a7
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-09 12:08:46 +0100

    FIX - Calendars from db
    Now the collection is typed and the data accesed and stored on a vector

--- src/data_access/get_calendars_from_db.rs
+++ src/data_access/get_calendars_from_db.rs
@@ -1,6 +1,9 @@
-use mongodb::bson::{doc, Document};
-use mongodb::{Client, Collection};
+use futures::stream::TryStreamExt;
+use mongodb::bson::doc;
+use mongodb::Client;
 use serde::{Deserialize, Serialize};
+
+use crate::domain::models::calendar_to_save::CalendarToSave;
 
 #[derive(Serialize, Deserialize)]
 struct CalendarModel {
@@ -14,59 +17,15 @@ pub async fn get_calendars_from_db() -> Result<(), mongodb::error::Error> {
 
     let client = Client::with_uri_str(connection_string).await?;
     let database = client.database(db_name);
-    let collection: Collection<Document> = database.collection(collection_name);
+    let collection = database.collection::<CalendarToSave>(collection_name);
 
     let filter = doc! {};
-    let cursor = collection.find(filter, None).await;
+    let mut cursor = collection.find(filter, None).await?;
 
-    match cursor {
-        Ok(mongo_document) => {
-            let bson_raw = mongo_document.current();
-            let raw_buf = bson_raw.to_raw_document_buf();
-            dbg!(raw_buf);
-            // dbg!(bson_documet);
-        }
-        Err(_) => {
-            println!("Erro on formating");
-        }
+    let mut calendars: Vec<CalendarToSave> = vec![];
+    while let Some(calendar) = cursor.try_next().await? {
+        calendars.push(calendar);
     }
 
     Ok(())
-
-    // let mut documents: Vec<Document> = Vec::new();
-    // while let Some(result) = cursor.await {
-    //     match result {
-    //         Ok(document) => documents.push(document),
-    //         Err(e) => return Err(e.into()), // Handle the error
-    //     }
-    // }
-
-    // let document = match cursor.await {
-    //     Ok(document_cursor) => {
-    //         dbg!(&document_cursor);
-    //         mongodb::bson::from_bson(bson::Bson::Document(document_cursor[0]))
-    //     }
-    //     Err(err) => {
-    //         println!("Error");
-    //         err
-    //     }
-    //     _ => {
-    //         println!("None");
-    //     }
-    // };
-
-    // for calendar in cursor {
-    //     match calendar {
-    //         Ok(document) => {
-    //             match bson::from_bson<CalendarModel>(bson::Bson::Document(document)) {
-    //                 Ok(calendar_model) => {calendar_model}
-    //                 Err(e) => {e}
-
-    //             }
-    //         }
-    //         Err(err) => {err}
-
-    //     }
-    //     Ok(calendar);
-    // }
 }

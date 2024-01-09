23d40c885c90ee2737d77d4b76de0536b9cffa21
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-09 12:07:49 +0100

    ADD - Debug implementation to Calendar to Save

--- src/domain/models/calendar_to_save.rs
+++ src/domain/models/calendar_to_save.rs
@@ -2,15 +2,15 @@ use serde::{Deserialize, Serialize};
 
 use crate::domain::models::event::Event;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct CalendarToSave {
     pub calendar_date: String,
     pub calendar_id: CalendarId,
-    pub events: [Event; 24],
+    // pub events: [Event; 24],
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct CalendarId(String);
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 struct UserId(String);

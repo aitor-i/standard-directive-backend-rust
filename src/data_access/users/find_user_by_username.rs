eb1b9d4447c815710aa1d4e69d20ba281119ec76
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-12 16:04:17 +0100

    UPDATE - Find username by id

--- src/data_access/users/find_user_by_username.rs
+++ src/data_access/users/find_user_by_username.rs
@@ -3,7 +3,9 @@ use mongodb::bson::doc;
 
 use mongodb::Client;
 
-pub async fn find_user_by_id(id: &String) -> Result<Option<User>, mongodb::error::Error> {
+pub async fn find_user_by_username(
+    username: &String,
+) -> Result<Option<User>, mongodb::error::Error> {
     let connection_string = "mongodb://localhost:27017";
     let db_name = "standard_directive";
     let collection_name = "users";
@@ -12,7 +14,7 @@ pub async fn find_user_by_id(id: &String) -> Result<Option<User>, mongodb::error
     let database = client.database(db_name);
     let collection = database.collection::<User>(collection_name);
 
-    let filter = doc! { "user_id": id};
+    let filter = doc! { "username": username};
     let user = collection.find_one(filter, None).await?;
 
     Ok(user)
--- src/data_access/users/mod.rs
+++ src/data_access/users/mod.rs
@@ -1,3 +1,3 @@
 pub mod add_user_db;
-pub mod find_user_by_id;
+pub mod find_user_by_username;
 pub mod is_username_free;

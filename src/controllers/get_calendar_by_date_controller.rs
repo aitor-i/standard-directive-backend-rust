a41a6c1ea908a9616397741c2c91e6f7909b36f4
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-14 11:32:18 +0100

    ADD - Now returns calendars by username and date

--- src/controllers/get_calendar_by_date_controller.rs
+++ src/controllers/get_calendar_by_date_controller.rs
@@ -2,26 +2,32 @@ use serde::Deserialize;
 use warp::{Filter, Rejection, Reply};
 
 use crate::{
-    application::convert_string_to_date::convert_string_to_date,
+    application::{convert_string_to_date::convert_string_to_date, validate_token::validate_token},
     data_access::get_calendar_by_date::get_calendar_by_date,
 };
 
 #[derive(Deserialize)]
 struct QueryParams {
     date: String,
-    user_id: String,
+    token: String,
 }
 
 async fn request_mapper(params: QueryParams) -> Result<impl Reply, Rejection> {
-    let date = params.date;
+    let date = params.date.clone();
 
-    let db_res = get_calendar_by_date(date, params.user_id).await;
-    match db_res {
-        Ok(calendar) => Ok(warp::reply::json(&calendar)),
-        Err(err) => {
-            let error_message = err.to_string();
-            Ok(warp::reply::json(&error_message))
+    match validate_token(&params.token) {
+        Ok(token) => {
+            println!("username: {}, date: {}", &token.username, &date);
+            let db_res = get_calendar_by_date(date, token.username).await;
+            match db_res {
+                Ok(calendar) => Ok(warp::reply::json(&calendar)),
+                Err(err) => {
+                    let error_message = err.to_string();
+                    Ok(warp::reply::json(&error_message))
+                }
+            }
         }
+        Err(err) => Ok(warp::reply::json(&err.to_string())),
     }
 }
 

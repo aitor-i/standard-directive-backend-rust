db9b17eb9f67a274b6910a570ec872f1f50aef0c
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-14 01:04:19 +0100

    ADD - Calendar struct

---
+++ src/domain/models/calendar.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Calendar {
+    color: Option<String>,
+    eventName: String,
+    eventPosition: Option<String>,
+    hour: u8,
+    hourDisplay: String,
+    taskId: Option<u16>,
+}

78c2d804d30fc93e879101ec2110f198e0706b72
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-14 11:31:15 +0100

    UPDATE - get calendar by date filters by username

--- src/data_access/get_calendar_by_date.rs
+++ src/data_access/get_calendar_by_date.rs
@@ -1,24 +1,23 @@
-use chrono::NaiveDateTime;
 use mongodb::bson::doc;
 use mongodb::Client;
 
-use crate::domain::models::calendar_to_save::CalendarToSave;
+use crate::domain::models::calendar_db::CalendarDb;
 
 pub async fn get_calendar_by_date(
     date: String,
     user_id: String,
-) -> Result<Option<CalendarToSave>, mongodb::error::Error> {
+) -> Result<Option<CalendarDb>, mongodb::error::Error> {
     let connection_string = "mongodb://localhost:27017";
     let db_name = "standard_directive";
     let collection_name = "calendars";
 
     let client = Client::with_uri_str(connection_string).await?;
     let database = client.database(db_name);
-    let collection = database.collection::<CalendarToSave>(collection_name);
+    let collection = database.collection::<CalendarDb>(collection_name);
 
     dbg!(&date.to_string());
 
-    let filter = doc! {"calendar_date": &date.to_string(), "user_id": user_id};
+    let filter = doc! {"calendar_date": &date.to_string(), "username": user_id};
     let calendar = collection.find_one(filter, None).await?;
 
     Ok(calendar)
--- src/data_access/mod.rs
+++ src/data_access/mod.rs
@@ -1,4 +1,5 @@
 pub mod add_event_to_db;
 pub mod get_calendar_by_date;
 pub mod get_calendars_from_db;
+pub mod update_calendar;
 pub mod users;

f263b7067fd870568d1cc611e5d8ad227de318e1
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-16 17:12:49 +0100

    ADD - update password to router

--- src/routers/users_router.rs
+++ src/routers/users_router.rs
@@ -2,7 +2,12 @@ use warp::Filter;
 
 use crate::controllers::users::login_controller::login_controller;
 use crate::controllers::users::register_user_controller::register_user_controller;
+use crate::controllers::users::update_password_controller::update_password_controller;
 
 pub fn users_router() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path("users").and(register_user_controller().or(login_controller()))
+    warp::path("users").and(
+        register_user_controller()
+            .or(login_controller())
+            .or(update_password_controller()),
+    )
 }

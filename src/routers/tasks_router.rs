33230d9103f87203836892d41c64767bbf77deac
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-20 17:32:43 +0100

    ADD - fet tasks to taks router

--- src/routers/tasks_router.rs
+++ src/routers/tasks_router.rs
@@ -1,10 +1,9 @@
 use warp::{reject::Rejection, reply::Reply, Filter};
 
-use crate::controllers::{
-    get_calendars_controller::get_calendars_controller,
-    tasks::post_add_task_controller::post_add_task_controller,
+use crate::controllers::tasks::{
+    get_tasks_controller::get_tasks_controller, post_add_task_controller::post_add_task_controller,
 };
 
 pub fn tasks_router() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    warp::path("tasks").and(post_add_task_controller().or(get_calendars_controller()))
+    warp::path("tasks").and(post_add_task_controller().or(get_tasks_controller()))
 }

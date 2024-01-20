0f722d7abc022a70a19d22706ba6800a227b7a89
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-20 17:31:43 +0100

    ADD - Tasks from db

---
+++ src/data_access/tasks/get_tasks_from_db.rs
@@ -0,0 +1,21 @@
+use bson::doc;
+use mongodb::Client;
+
+use crate::domain::models::save_tasks_view_model::TaskDbModel;
+
+pub async fn get_tasks_from_db(
+    username: String,
+) -> Result<Option<TaskDbModel>, mongodb::error::Error> {
+    let connection_string = "mongodb://localhost:27017";
+    let db_name = "standard_directive";
+    let collection_name = "calendars";
+
+    let client = Client::with_uri_str(connection_string).await?;
+    let database = client.database(db_name);
+    let collection = database.collection::<TaskDbModel>(collection_name);
+
+    let filter = doc! {"username": username};
+    let tasks_model = collection.find_one(filter, None).await?;
+
+    Ok(tasks_model)
+}
--- src/data_access/tasks/mod.rs
+++ src/data_access/tasks/mod.rs
@@ -1,1 +1,2 @@
 pub mod add_task_to_db;
+pub mod get_tasks_from_db;

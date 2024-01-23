966c65a5e0b894737915d2d1cbeb2bee04596b82
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-23 20:25:47 +0200

    ADD - Env var for togin token generation

--- src/application/generate_token.rs
+++ src/application/generate_token.rs
@@ -1,9 +1,14 @@
+use dotenv::dotenv;
 use jsonwebtoken::{encode, errors::Error, EncodingKey, Header};
+use std::env;
 
 use crate::domain::models::auth_token::AuthToken;
 
 pub fn generate_token(user_token: AuthToken) -> Result<String, Error> {
-    let encoding_key = EncodingKey::from_secret("test_secret".as_ref());
+    dotenv().ok();
+
+    let secret = env::var("LOGIN_TOKEN_KEY").expect("Error geting token key");
+    let encoding_key = EncodingKey::from_secret(secret.as_ref());
 
     encode(&Header::default(), &user_token, &encoding_key)
 }
--- src/application/validate_token.rs
+++ src/application/validate_token.rs
@@ -1,9 +1,13 @@
+use dotenv::dotenv;
 use jsonwebtoken::{decode, DecodingKey, Validation};
+use std::env;
 
 use crate::domain::models::auth_token::AuthToken;
 
 pub fn validate_token(token: &str) -> Result<AuthToken, jsonwebtoken::errors::Error> {
-    let decoding_key = DecodingKey::from_secret("test_secret".as_ref());
+    dotenv().ok();
+    let secret = env::var("LOGIN_TOKEN_KEY").expect("Error geting token key");
+    let decoding_key = DecodingKey::from_secret(secret.as_ref());
 
     let validation = Validation::default();
 

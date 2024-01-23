00dbbb6e643f70fb73c4bc2a731bbfc18377a4ca
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-23 20:38:16 +0200

    ADD - Expiration time to login token

--- src/domain/models/auth_token.rs
+++ src/domain/models/auth_token.rs
@@ -1,10 +1,11 @@
+use chrono::{Duration, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct AuthToken {
     pub username: String,
     pub roles: Option<Roles>,
-    pub exp: usize,
+    pub exp: i64,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -12,10 +13,14 @@ pub struct Roles(Vec<String>);
 
 impl AuthToken {
     pub fn without_roles(username: String) -> AuthToken {
+        let expiration_time = Utc::now()
+            .checked_add_signed(Duration::hours(24))
+            .expect("valid timestamp")
+            .timestamp();
         AuthToken {
             username,
             roles: None,
-            exp: 10000000000,
+            exp: expiration_time,
         }
     }
 }

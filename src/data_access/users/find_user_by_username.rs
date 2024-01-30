713472a0d31d69d4fffb804dd4745b09e8d17c6b
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-30 21:27:34 +0100

    RESTORE - find user

--- src/data_access/users/find_user_by_username.rs
+++ src/data_access/users/find_user_by_username.rs
@@ -4,7 +4,6 @@ use mongodb::bson::doc;
 use mongodb::Client;
 
 use dotenv::dotenv;
-use mongodb::options::ClientOptions;
 use std::env;
 pub async fn find_user_by_username(
     username: &String,
@@ -15,11 +14,7 @@ pub async fn find_user_by_username(
     let db_name = "standard_directive";
     let collection_name = "users";
 
-    let mut client_options = ClientOptions::parse_async(connection_string).await?;
-    client_options.max_pool_size = Some(20);
-
-    let client = Client::with_options(client_options)?;
-
+    let client = Client::with_uri_str(connection_string).await?;
     let database = client.database(db_name);
     let collection = database.collection::<User>(collection_name);
 

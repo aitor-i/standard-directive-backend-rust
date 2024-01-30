f90b8c3bf02db9d37db6d8df58951f43dec1d105
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-01-30 20:59:48 +0100

    UPDATE - find user by name
    ADD connection pool settings

--- src/data_access/users/find_user_by_username.rs
+++ src/data_access/users/find_user_by_username.rs
@@ -4,6 +4,7 @@ use mongodb::bson::doc;
 use mongodb::Client;
 
 use dotenv::dotenv;
+use mongodb::options::ClientOptions;
 use std::env;
 pub async fn find_user_by_username(
     username: &String,
@@ -14,7 +15,11 @@ pub async fn find_user_by_username(
     let db_name = "standard_directive";
     let collection_name = "users";
 
-    let client = Client::with_uri_str(connection_string).await?;
+    let mut client_options = ClientOptions::parse_async(connection_string).await?;
+    client_options.max_pool_size = Some(20);
+
+    let client = Client::with_options(client_options)?;
+
     let database = client.database(db_name);
     let collection = database.collection::<User>(collection_name);
 

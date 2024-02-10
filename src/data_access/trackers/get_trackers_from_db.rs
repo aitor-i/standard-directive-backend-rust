0ab6a94e02e297ab032c9ff2f48116a9e28dbaf4
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-02-10 16:42:55 +0100

    ADD - Get trackers form db

---
+++ src/data_access/trackers/get_trackers_from_db.rs
@@ -0,0 +1,25 @@
+use bson::doc;
+use mongodb::Client;
+
+use dotenv::dotenv;
+use std::env;
+
+use crate::domain::models::trackers::TrackersDB;
+pub async fn get_trackers_from_db(
+    username: String,
+) -> Result<Option<TrackersDB>, mongodb::error::Error> {
+    dotenv().ok();
+
+    let connection_string = env::var("CONNECTION_STRING").expect("DATABASE_URL must be set");
+    let db_name = "standard_directive";
+    let collection_name = "tasks";
+
+    let client = Client::with_uri_str(connection_string).await?;
+    let database = client.database(db_name);
+    let collection = database.collection::<TrackersDB>(collection_name);
+
+    let filter = doc! {"username": username};
+    let trackers_model = collection.find_one(filter, None).await?;
+
+    Ok(trackers_model)
+}
---
+++ src/data_access/trackers/mod.rs
@@ -0,0 +1,2 @@
+pub mod add_trackers_db;
+pub mod get_trackers_from_db;

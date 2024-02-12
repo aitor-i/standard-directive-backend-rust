4e2995889b336cf40a7d03072c0ba4bc5849bbcc
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-02-12 20:51:09 +0100

    FIX - Add new trackers

--- src/data_access/trackers/add_trackers_db.rs
+++ src/data_access/trackers/add_trackers_db.rs
@@ -9,6 +9,10 @@ use std::env;
 pub async fn add_tracker_to_db(trackers: TrackersDB) -> mongodb::error::Result<()> {
     dotenv().ok();
 
+    if trackers.trackers.len() == 0 {
+        return Ok(());
+    }
+
     let connection_string = env::var("CONNECTION_STRING").expect("DATABASE_URL must be set");
     let db_name = "standard_directive";
     let collection_name = "trackers";
@@ -16,19 +20,23 @@ pub async fn add_tracker_to_db(trackers: TrackersDB) -> mongodb::error::Result<(
     let client = Client::with_uri_str(connection_string).await?;
     let database = client.database(db_name);
     let collection: Collection<Document> = database.collection(collection_name);
+    let insert_collection: Collection<TrackersDB> = database.collection(collection_name);
 
-    let filter = doc! { "username": trackers.username   };
+    let filter = doc! { "username": trackers.username.clone()   };
 
     match bson::to_bson(&trackers.trackers) {
         Ok(trackers_bson) => {
             let update = doc! { "$set": { "trackers": trackers_bson} };
 
             match collection.update_one(filter, update, None).await {
-                Ok(colletion) => {
+                Ok(colletion_res) => {
                     println!("Update");
-                    if colletion.matched_count == 0 {
-                        let custom_error = Error::custom("No tracker found");
-                        return Err(custom_error);
+                    if colletion_res.matched_count == 0 {
+                        println!("No matched trackers");
+                        // let custom_error = Error::custom("No tracker found");
+
+                        let _ = insert_collection.insert_one(trackers, None).await;
+                        return Ok(());
                     };
                     return Ok(());
                 }
